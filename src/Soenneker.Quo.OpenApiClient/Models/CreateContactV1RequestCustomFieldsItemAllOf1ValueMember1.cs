f40c4bc465e0a940c8416ced8228e6c50239a766
Jake Soenneker <4441470+soenneker@users.noreply.github.com>
2026-08-13 17:53:57 +0000

    Automated update

--- src/Soenneker.Quo.OpenApiClient/Models/CreateContactV1RequestCustomFieldsItemAllOf1ValueMember1.cs
+++ src/Soenneker.Quo.OpenApiClient/Models/CreateContactV1RequestCustomFieldsItemAllOf1ValueMember1.cs
@@ -9,27 +9,27 @@ namespace Soenneker.Quo.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class GetCallTranscriptV1200ResponseDataDialogue : IAdditionalDataHolder, IParsable
+    public partial class CreateContactV1RequestCustomFieldsItemAllOf1ValueMember1 : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Quo.OpenApiClient.Models.GetCallTranscriptV1200ResponseDataDialogue"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.Quo.OpenApiClient.Models.CreateContactV1RequestCustomFieldsItemAllOf1ValueMember1"/> and sets the default values.
         /// </summary>
-        public GetCallTranscriptV1200ResponseDataDialogue()
+        public CreateContactV1RequestCustomFieldsItemAllOf1ValueMember1()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Quo.OpenApiClient.Models.GetCallTranscriptV1200ResponseDataDialogue"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Quo.OpenApiClient.Models.CreateContactV1RequestCustomFieldsItemAllOf1ValueMember1"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Quo.OpenApiClient.Models.GetCallTranscriptV1200ResponseDataDialogue CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.Quo.OpenApiClient.Models.CreateContactV1RequestCustomFieldsItemAllOf1ValueMember1 CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Quo.OpenApiClient.Models.GetCallTranscriptV1200ResponseDataDialogue();
+            return new global::Soenneker.Quo.OpenApiClient.Models.CreateContactV1RequestCustomFieldsItemAllOf1ValueMember1();
         }
         /// <summary>
         /// The deserialization information for the current model
--- src/Soenneker.Quo.OpenApiClient/Models/ListContactsV1200ResponseDataItemCustomFieldsItemAllOf1ValueMember1.cs
+++ src/Soenneker.Quo.OpenApiClient/Models/ListContactsV1200ResponseDataItemCustomFieldsItemAllOf1ValueMember1.cs
@@ -9,27 +9,27 @@ namespace Soenneker.Quo.OpenApiClient.Models
 {
     [global::System.CodeDom.Compiler.GeneratedCode("Kiota", "1.0.0")]
     #pragma warning disable CS1591
-    public partial class GetCallSummaryV1200ResponseDataJobs : IAdditionalDataHolder, IParsable
+    public partial class ListContactsV1200ResponseDataItemCustomFieldsItemAllOf1ValueMember1 : IAdditionalDataHolder, IParsable
     #pragma warning restore CS1591
     {
         /// <summary>Stores additional data not described in the OpenAPI description found when deserializing. Can be used for serialization as well.</summary>
         public IDictionary<string, object> AdditionalData { get; set; }
         /// <summary>
-        /// Instantiates a new <see cref="global::Soenneker.Quo.OpenApiClient.Models.GetCallSummaryV1200ResponseDataJobs"/> and sets the default values.
+        /// Instantiates a new <see cref="global::Soenneker.Quo.OpenApiClient.Models.ListContactsV1200ResponseDataItemCustomFieldsItemAllOf1ValueMember1"/> and sets the default values.
         /// </summary>
-        public GetCallSummaryV1200ResponseDataJobs()
+        public ListContactsV1200ResponseDataItemCustomFieldsItemAllOf1ValueMember1()
         {
             AdditionalData = new Dictionary<string, object>();
         }
         /// <summary>
         /// Creates a new instance of the appropriate class based on discriminator value
         /// </summary>
-        /// <returns>A <see cref="global::Soenneker.Quo.OpenApiClient.Models.GetCallSummaryV1200ResponseDataJobs"/></returns>
+        /// <returns>A <see cref="global::Soenneker.Quo.OpenApiClient.Models.ListContactsV1200ResponseDataItemCustomFieldsItemAllOf1ValueMember1"/></returns>
         /// <param name="parseNode">The parse node to use to read the discriminator value and create the object</param>
-        public static global::Soenneker.Quo.OpenApiClient.Models.GetCallSummaryV1200ResponseDataJobs CreateFromDiscriminatorValue(IParseNode parseNode)
+        public static global::Soenneker.Quo.OpenApiClient.Models.ListContactsV1200ResponseDataItemCustomFieldsItemAllOf1ValueMember1 CreateFromDiscriminatorValue(IParseNode parseNode)
         {
             if(ReferenceEquals(parseNode, null)) throw new ArgumentNullException(nameof(parseNode));
-            return new global::Soenneker.Quo.OpenApiClient.Models.GetCallSummaryV1200ResponseDataJobs();
+            return new global::Soenneker.Quo.OpenApiClient.Models.ListContactsV1200ResponseDataItemCustomFieldsItemAllOf1ValueMember1();
         }
         /// <summary>
         /// The deserialization information for the current model
